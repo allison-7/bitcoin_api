801bf95d9056b80beef638fe7ba90eae146cb9d8
Mitchell Hynes <me@mitchellhynes.com>
2021-05-27 21:30:30 -0230

    Initialized API

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  const data = await fetch(
+    "https://api.coinbase.com/v2/prices/spot?currency=USD"
+  );
+
+  const text = await data.text();
+
+  console.log(text);
+
+  res.send(`Name Is: ${req.query.name}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
